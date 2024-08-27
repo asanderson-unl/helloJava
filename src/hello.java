a60d158b73cf60a3cacf702cea14f045967c22bd
alex <aschwarz@xephyr.xyz>
2024-08-27 11:58:50 -0500

    added alex schwarz

--- src/hello.java
+++ src/hello.java
@@ -2,6 +2,8 @@ public class hello {
     public static void main(String[] args) {
         System.out.println("Alexander Sanderson!");
         System.out.println("Computer Science");
+        System.out.println("Alex Schwarz!!");
+        System.out.println("Computer Science");
     }
 
 }
